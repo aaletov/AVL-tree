cfba8660d9c7fdabce21258783ba7b7ee877aa02
aaletov <52921276+aaletov@users.noreply.github.com>
2021-05-03 23:21:23 +0300

    Rotations v1

--- AVL-tree/AVL-tree.hpp
+++ AVL-tree/AVL-tree.hpp
@@ -23,13 +23,18 @@ private:
 	{
 		node_t* left_;
 		node_t* right_;
+		// Высота правого - высота левого
 		int bal_;
 		T key_;
 		int count_;
 	};
+	//  Малое левое вращение
 	static void doRRrotation(node_t* root);
+	// Малое правое вращение
 	static void doLLrotation(node_t* root);
+	// Большое правое вращение
 	static void doLRrotation(node_t* root);
+	// Большое левое вращение
 	static void doRLrotation(node_t* root);
 	static void print(node_t* root);
 	node_t* root_;
@@ -80,27 +85,61 @@ void AVL_tree< T, Compare >::inorderTraversal(Callable callback)
 }
 
 template < class T, class Compare >
-static void AVL_tree< T, Compare >::doRRrotation(node_t* root)
+static void AVL_tree< T, Compare >::doRRrotation(node_t* p)
 {
-
+	assert(p != nullptr);
+	assert(p->right_ != nullptr);
+	node_t* p1 = p->right_;
+	p->right_ = p1->left_;
+	p1->left_ = p;
+	//
+	p->bal_ = 0;
 }
 
 template < class T, class Compare >
-static void AVL_tree< T, Compare >::doLLrotation(node_t* root)
+static void AVL_tree< T, Compare >::doLLrotation(node_t* p)
 {
-
+	assert(root != nullptr);
+	assert(root->left_ != nullptr);
+	node_t* p1 = p->left_;
+	p->left_ = p1->right_;
+	p1->right_ = p;
+	//
+	p->bal_ = 0;
 }
 
 template < class T, class Compare >
-static void AVL_tree< T, Compare >::doLRrotation(node_t* root)
+static void AVL_tree< T, Compare >::doLRrotation(node_t* p)
 {
-
+	assert(p != nullptr);
+	assert(p->left_ != nullptr);
+	assert(p->left_->right_ != nullptr);
+	node_t* p1 = p->left_;
+	node_t* p2 = p1->right_;
+	p1->right_ = p2->left_;
+	p2->left_ = p1;
+	p->left_ = p2->right_;
+	p2->right_ = p;
+	//
+	p->bal = (p2->bal == -1) ? 1 : 0;
+	p1->bal = (p2->bal == 1) ? -1 : 0;
 }
 
 template < class T, class Compare >
-static void AVL_tree< T, Compare >::doRLrotation(node_t* root)
+static void AVL_tree< T, Compare >::doRLrotation(node_t* p)
 {
-
+	assert(p != nullptr);
+	assert(p->right_ != nullptr);
+	asster(p->right_->left_ != nullptr);
+	node_t* p1 = p->right_;
+	node_t* p2 = p1->left_;
+	p1->left_ = p2->right_;
+	p2->right_ = p1;
+	p->right_ = p2->left_;
+	p2->left_ = p;
+	//
+	p->bal_ = (p2->bal_ == 1) ? -1 : 0;
+	p1->bal_ = (p2->bal == -1) ? 1 : 0;
 }
 
 template < class T, class Compare >
